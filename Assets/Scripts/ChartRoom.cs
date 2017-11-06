b7062f150b1fe113677e95b0bb2d1c98b331a12e
笛小纸 <woodyplus@dixiaozhiMacBookPro.local>
2017-11-06 13:24:45 +0800

    Fixing commit

---
+++ Assets/Scripts/ChartRoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChartRoom 
+{
+	static ChartRoom _Instance = null;
+	static string _ChartText = null;
+
+	public static ChartRoom GetInstance ()
+	{
+		if (null == _Instance) 
+		{
+			_Instance = new ChartRoom ();
+		}
+		return _Instance;
+	}
+
+	ChartRoom()
+	{
+		_ChartText = "";
+
+		return;
+	}
+
+	public string GetChartText()
+	{
+		return _ChartText;
+	}
+
+	public void AppendChartText(string chartText)
+	{
+		_ChartText = _ChartText + "\n" + chartText;
+
+		return;
+	}
+
+	public void ClearChartText()
+	{
+		_ChartText = "";
+
+		return;
+	}
+}
